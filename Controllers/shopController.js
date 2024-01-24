c1b83151bb6fd6fd34901e1a705d44fa60495724
mr. whalen <michael.whalen@sitechhs.com>
2024-01-24 10:50:00 -0500

    ready

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -1,7 +1,29 @@
+exports.middlewareSample = (req, res, next) => {
+  req.name = "TEST"; //this runs before the response is sent to the user
+  next(); //cues the next step of the response to occur
+};
+//for the next example we need to set up some additional middleware in app.js to allow the requests body to be readable
+exports.authMiddleware = (req, res, next) => {
+  if (req.body.user) {
+    next();
+  } else {
+    //throw new Error("No user");
+    res.json("you must be signed in");
+  }
+};
+exports.authPage = (req, res, next) => {
+  try {
+    res.json(req.body.user);
+  } catch (error) {
+    console.log(error);
+    res.json("User not signed in");
+  }
+};
 exports.homePage = (req, res) => {
   const stores = ["Dunkin", "Tim Hortons", "Starbucks"];
   try {
-    res.json(stores);
+    console.log(req.name); //we get req.name from the middleware but WE MUST call it in index
+    res.json([stores, req.name]); //if we want to send multiple "things" back to the user we need to use an array or object. Can't simply use , as that denotes status codes
   } catch (error) {
     console.log(error);
   }
--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 const routes = require("./Routes/index");
-//routes, imported from routes folder above
-
+//takes raw requests and turns them into usable properties on req.body
+app.use(express.json());
+app.use(express.urlencoded());
 app.use("/", routes);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
