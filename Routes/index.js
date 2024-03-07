90be62552bbb1cbb02c41ace56874ba16e5b84fe
mr. whalen <michael.whalen@sitechhs.com>
2024-03-07 11:00:32 -0500

    ghjkgh

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/shopController");
+const authController = require("../Controllers/authController");
 router.get("/", shopController.homePage);
 router.post("/add", shopController.createShop);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/protected", authController.authCheck, authController.protected);
 module.exports = router;
