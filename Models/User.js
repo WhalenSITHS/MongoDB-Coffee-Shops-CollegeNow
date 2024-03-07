84e3971bcf16a31c3a75dbd798ed50abfca7a01d
mr. whalen <michael.whalen@sitechhs.com>
2024-03-07 15:27:10 -0500

    gfhdgh

---
+++ Models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
